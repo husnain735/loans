c69e82e8559f0c4635d4ec23f82114fb8d1f84a8
Haris Iqbal <harisiqbal1998@yahoo.com>
2022-12-11 20:11:44 +0500

    applicant model

---
+++ src/app/models/application.ts
@@ -0,0 +1,12 @@
+export class Application{
+    ApplicationId:number;
+    ID:number;
+    EmailAddress:string;
+    LoansBrokerId:number;
+    ReasonIDS:string;
+    CreatedBy:number;
+    ModifiedBy:number;
+    CreatedDate:Date;
+    ModifiedDate:Date;
+    IsDeleted:boolean;
+}
