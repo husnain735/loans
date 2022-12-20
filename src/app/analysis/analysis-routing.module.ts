9774d23654ccdcf9ff177143a2852d6870dc7a5f
husnain mughal <husnainijaz735@gmail.com>
2022-12-21 00:20:03 +0500

    all routing added and started working Liabilities

--- src/app/analysis/analysis-routing.module.ts
+++ src/app/analysis/analysis-routing.module.ts
@@ -4,6 +4,14 @@ import { AnalysisComponent } from './analysis/analysis.component';
 import { ApplicantDetailComponent } from './applicant-detail/applicant-detail.component';
 import { ApplicantsComponent } from './applicants/applicants.component';
 import { AssetsComponent } from './assets/assets.component';
+import { ExpensesComponent } from './expenses/expenses.component';
+import { FinancialGoalsComponent } from './financial-goals/financial-goals.component';
+import { LiabilitiesComponent } from './liabilities/liabilities.component';
+import { ReasonApplicationComponent } from './reason-application/reason-application.component';
+import { RelativeComponent } from './relative/relative.component';
+import { RetirementPlansComponent } from './retirement-plans/retirement-plans.component';
+import { ReviewComponent } from './review/review.component';
+import { RiskInsuranceProfileComponent } from './risk-insurance-profile/risk-insurance-profile.component';
 
 const routes: Routes = [
   {
@@ -21,6 +29,38 @@ const routes: Routes = [
       {
         path: ':guid/assets',
         component: AssetsComponent
+      },
+      {
+        path: ':guid/liabilities',
+        component: LiabilitiesComponent
+      },
+      {
+        path: ':guid/reason-application',
+        component: ReasonApplicationComponent
+      },
+      {
+        path: ':guid/relative',
+        component: RelativeComponent
+      },
+      {
+        path: ':guid/risk-insurance-profile',
+        component: RiskInsuranceProfileComponent
+      },
+      {
+        path: ':guid/retirement-plans',
+        component: RetirementPlansComponent
+      },
+      {
+        path: ':guid/financial-goals',
+        component: FinancialGoalsComponent
+      },
+      {
+        path: ':guid/expenses',
+        component: ExpensesComponent
+      },
+      {
+        path: ':guid/review',
+        component: ReviewComponent
       }
     ]
   }
--- src/app/analysis/liabilities/liabilities.component.ts
+++ src/app/analysis/liabilities/liabilities.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared/services/shared.service';
 
 @Component({
   selector: 'app-liabilities',
   templateUrl: './liabilities.component.html',
   styleUrls: ['./liabilities.component.scss']
 })
-export class LiabilitiesComponent {
+export class LiabilitiesComponent implements OnInit {
 
+  IsNext = false;
+  constructor(public _sharedService: SharedService) {
+
+  }
+  ngOnInit(){
+
+  }
 }
