079fd3b92c6bef19c4767afe00ff6c9c976fb888
Haris Iqbal <harisiqbal1998@yahoo.com>
2023-01-02 20:21:38 +0500

    retirement plans and financial plans

--- src/app/analysis/financial-goals/financial-goals.component.ts
+++ src/app/analysis/financial-goals/financial-goals.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./financial-goals.component.scss']
 })
 export class FinancialGoalsComponent {
+  textarea:string;
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.textarea = undefined;
+  }
+
+  RetirementIncome = [
+    "$40,000","$50,000","$60,000","$70,000","$80,000"
+  ];
 
 }
