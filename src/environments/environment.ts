e0012b0c562710c3a824ae1b2ce56cc0a3006fd9
RyzenWTF735 <husnainijaz735@gmail.com>
2023-05-27 12:00:51 -0700

    push

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@ export const environment = {
     ResourceServer: {
         Endpoint: "https://localhost:44321/api/",
         BaseApiUrl: "https://localhost:44321",
-        // Endpoint: "http://loansapi.financeprospects.loans/api/"
+        //BaseApiUrl: "http://loansapi.financeprospects.loans",
+        //Endpoint: "http://loansapi.financeprospects.loans/api/"
     },
     key: {
       password: '192.168.10.1'
